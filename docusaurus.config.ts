4965a21149372fc981cf7f2da8e1c759cac09b56
Gareth Andrew <gingerhendrix@gmail.com>
2024-08-14 23:31:19 +0100

    Fix publish config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,8 +16,9 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'autocode2', // Usually your GitHub org/user name.
+  projectName: 'autocode2.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
