95a63f1dd4f4daf9a2347d64eadc94e46afe285d
Gareth Andrew <gingerhendrix@gmail.com>
2025-01-20 21:52:09 +0000

    Homepage under construction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <img src="/img/auto-code-2-header-svg.svg" width="100%" />
-    </header>
+    <a href="/blog">
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <img src="/img/auto-code-2-header-svg.svg" width="100%" />
+      </header>
+    </a>
   );
 }
 
@@ -21,10 +21,13 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Automatic coding with AI">
+      description="Automatic coding with AI"
+    >
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <div style={{ textAlign: 'center', margin: 'auto', paddingTop: '4em' }}>
+          Under construction
+        </div>
       </main>
     </Layout>
   );
